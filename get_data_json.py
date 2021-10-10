338f2a14001bfc55407af91ea465e44fa5721fa8
«Irene <pire66@mail.ru>
2021-10-10 19:18:48 +0300

    Create folder and file json

---
+++ get_data_json.py
@@ -0,0 +1,25 @@
+# python get_data_json.py
+import sys
+import requests
+import json
+import os
+import shutil
+monet = "aave" 
+days = 365
+urlRequest = "https://api.coingecko.com/api/v3/coins/"+monet+"/ohlc?vs_currency=usd&days="+ str(days)
+APIanswer = requests.get(urlRequest)
+namedir = monet+" for "+str(days)+" days"
+filename=monet+"_OHLC_"+str(days)+".json"
+filewithpath = namedir + "/" + filename
+try:
+    os.remove(filewithpath)
+except OSError:
+    pass
+try:
+    shutil.rmtree(namedir)
+except OSError:
+    pass
+os.mkdir(namedir)
+file_result = APIanswer.text
+with open(filewithpath, "w") as fi:
+    fi.write(file_result)
