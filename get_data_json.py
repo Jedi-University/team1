5c44552090a2c90f8de399795d632f40f6d814b3
Pavel Dudkov <pavel.dudkov@icloud.com>
2021-10-12 02:41:42 +0400

    Change .gitignore and get_data_json.py
    In the .gitignore add new path when will put extracred data.
    In the get_data_json.py create two function which return currect path on file
    with raw data.

--- get_data_json.py
+++ get_data_json.py
@@ -1,27 +1,42 @@
-# python get_data_json.py
 import sys
 import requests
 import json
 import os
 import shutil
-monet = "aave" 
-days = 365
-urlRequest = "https://api.coingecko.com/api/v3/coins/"+monet+"/ohlc?vs_currency=usd&days="+ str(days)
-APIanswer = requests.get(urlRequest)
-namedir = monet+"_for_"+str(days)+"_days"
-filename=monet+"_OHLC_"+str(days)+".json"
-filewithpath = namedir + "/" + filename
-try:
-    os.remove(filewithpath)
-except OSError:
-    pass
-try:
-    shutil.rmtree(namedir)
-except OSError:
-    pass
-os.mkdir(namedir)
-file_result = APIanswer.text
-with open(filewithpath, "w") as fi:
-    fi.write(file_result)
-#
 
+
+def get_data_and_write_to_file() -> str:
+    monet = "aave"
+    days = 365
+    namedir = monet + "_for_" + str(days) + "_days"
+    filename = monet + "_OHLC_" + str(days) + ".json"
+    filewithpath = namedir + "/" + filename
+    urlRequest = "https://api.coingecko.com/api/v3/coins/"+monet+"/ohlc?vs_currency=usd&days="+ str(days)
+    raw_data = requests.get(urlRequest).text
+
+    try:
+        os.remove(filewithpath)
+    except OSError:
+        pass
+    try:
+        shutil.rmtree(namedir)
+    except OSError:
+        pass
+
+    os.mkdir(namedir)
+    with open(filewithpath, "w") as fi:
+        fi.write(raw_data)
+
+    return filewithpath
+
+
+def start_get_data() -> str:
+    """Start get data by link and return current path on file with raw data"""
+
+    print("Start get data.")
+
+    result_path = get_data_and_write_to_file()
+
+    print(f"Raw data extracted and wrote by path '{result_path}'.")
+
+    return result_path
