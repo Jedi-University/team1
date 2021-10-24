57933dba92321a4e95bd266324588ae3574f2e4b
«Irene <pire66@mail.ru>
2021-10-24 11:40:09 +0300

    HomeWork 5: Class last

--- teachers_and_students.py
+++ teachers_and_students.py
@@ -25,9 +25,7 @@ class student(person):
     """Модель студента"""
     def __init__(self, name, age, gender,klassnumber, special):
         """Инициализирует атрибуты студента: номер класса,в котором учится студент, название специальности."""
-        self.name = name
-        self.age = age
-        self.gender = gender
+        super().__init__(name, age, gender)
         self.klassnumber = klassnumber
         self.special = special
 
@@ -39,9 +37,7 @@ class teacher(person):
     """Модель преподавателя"""
     def __init__(self, name, age, gender,subject, work):
         """Инициализирует атрибуты преподавателя: предмет, стаж работы."""
-        self.name = name
-        self.age = age
-        self.gender = gender
+        super().__init__(name, age, gender)
         self.subject = subject
         self.work = work
     
@@ -57,14 +53,14 @@ class teacher(person):
 
 t=98765
 print('t=',t)
+
 Studend1=student('Irene',25,'women','15a','mathematician')
-print(Studend1.tellyourname()," подумала: ",Studend1.think())
-print(Studend1.tellyourname()," сказала: ",Studend1.speak(t))
+print(Studend1.tellyourname(),"подумала: ",Studend1.think())
+print(Studend1.tellyourname(),"сказала: ",Studend1.speak(t))
 print(Studend1.sum(1256,76543))
 
 
 teacher1=teacher('Ben',45,'men','philosophy',15)
-print(teacher1.tellyourname()," подумал: ",Studend1.think())
-print(teacher1.tellyourname()," сказал: ",teacher1.speak(t))
-print(teacher1.tellyourname()," быстро посчитал: ",teacher1.answer(2))
+print(teacher1.tellyourname(),"подумал: ",Studend1.think())
+print(teacher1.tellyourname(),"сказал: ",teacher1.speak(t))
 
